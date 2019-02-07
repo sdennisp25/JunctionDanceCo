04a3802b3a2461cde93aa000f5e6511a2cc6be80
Dennis The Menace <sdennisp25@hotmail.com>
2019-02-07 14:03:14 -0700

    small changes to the about me page

--- junctiondanceco/src/pages/About/about.js
+++ junctiondanceco/src/pages/About/about.js
@@ -15,32 +15,21 @@ const About = props => (
         <div className="about">
           <h3>About Us</h3>
           <h6>Connecting Lives Through Performance and Outreach</h6>
-          <h6>
-            It sums up your unique promise of value. Your personal brand
-            statement is distinctive to you and you alone. ... The personal
-            brand statement is not a job title. A job title is what others will
-            try to classify you with, what employers and others want you to be
-            to fit you into a corporate setting. It sums up your unique promise
-            of value. Your personal brand statement is distinctive to you and
-            you alone. ... The personal brand statement is not a job title. A
-            job title is what others will try to classify you with, what
-            employers and others want you to be to fit you into a corporate
-            setting. It sums up your unique promise of value. Your personal
-            brand statement is distinctive to you and you alone. ... The
-            personal brand statement is not a job title. A job title is what
-            others will try to classify you with, what employers and others want
-            you to be to fit you into a corporate setting
+          <h6>Junction Dance Company is a diverse group of talented individuals that came together to unite all styles of dance. Passionately driven to spread the love for the art of dance by performing at charity events, benefit concerts, parades, and competitions. We strive to create unity in the dance community and by connecting others who have yet to experience the same passion.
           </h6>
         </div>
       </div>
       <Parallax
-        bgImage={require("./images/performance.jpeg")}
+        bgImage={require("./images/junctioncrew.jpeg")}
         bgImageAlt="the crew"
-        strength={200}
+				strength={150}
+				style={{
+					margin: "0 0 15% 0"
+				}}
       >
         <div
           style={{
-            height: "300px"
+						height: "400px",
           }}
         />
       </Parallax>
